37402d0824b5526724cf62113de68b4a2a46487e
boldr <idelrosario@boldrimpact.com>
2026-01-19 15:35:35 +0800

    chore: format all

--- apps/web/app/primevue/passthrough.ts
+++ apps/web/app/primevue/passthrough.ts
@@ -1,11 +1,11 @@
 import type { PrimeVuePTOptions } from "primevue/config";
 
 const GlobalPT: PrimeVuePTOptions = {
-  card: {
-    title: "font-semibold!",
-    caption: "mb-4",
-    footer: "mt-4",
-  },
+	card: {
+		title: "font-semibold!",
+		caption: "mb-4",
+		footer: "mt-4",
+	},
 };
 
 export default GlobalPT;
--- apps/web/app/primevue/theme.ts
+++ apps/web/app/primevue/theme.ts
@@ -4,30 +4,30 @@ import type { AuraBaseTokenSections } from "@primeuix/themes/aura/base";
 import type { Preset } from "@primeuix/themes/types";
 
 const MyPreset = definePreset(Aura, <Preset>{
-  semantic: <AuraBaseTokenSections.Semantic>{
-    primary: {
-      50: "{cyan.50}",
-      100: "{cyan.100}",
-      200: "{cyan.200}",
-      300: "{cyan.300}",
-      400: "{cyan.400}",
-      500: "{cyan.500}",
-      600: "{cyan.600}",
-      700: "{cyan.700}",
-      800: "{cyan.800}",
-      900: "{cyan.900}",
-      950: "{cyan.950}",
-    },
-  },
-  components: {},
+	semantic: <AuraBaseTokenSections.Semantic>{
+		primary: {
+			50: "{cyan.50}",
+			100: "{cyan.100}",
+			200: "{cyan.200}",
+			300: "{cyan.300}",
+			400: "{cyan.400}",
+			500: "{cyan.500}",
+			600: "{cyan.600}",
+			700: "{cyan.700}",
+			800: "{cyan.800}",
+			900: "{cyan.900}",
+			950: "{cyan.950}",
+		},
+	},
+	components: {},
 });
 
 export default {
-  preset: MyPreset,
-  options: {
-    darkModeSelector: ".p-dark",
-    cssLayer: {
-      order: "theme, base, primevue",
-    },
-  },
+	preset: MyPreset,
+	options: {
+		darkModeSelector: ".p-dark",
+		cssLayer: {
+			order: "theme, base, primevue",
+		},
+	},
 };
